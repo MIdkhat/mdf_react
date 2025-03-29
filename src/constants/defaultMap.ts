f09a3990a1d3091a423ac3de4d7d697afaa180aa
Midkhat I <midkhat.izmaylov@birdi.io>
2025-03-29 11:09:02 +1100

    updated map style

--- src/constants/defaultMap.ts
+++ src/constants/defaultMap.ts
@@ -1,7 +1,7 @@
 // src/constants/defaultMap.ts
 
 export const defaultMap = {
-    style: "https://demotiles.maplibre.org/style.json", // Default MapLibre style
+  style: "https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/openStreetMap.json",
     center: [144.9631, -37.8136] as [number, number], // Melbourne's coordinates [longitude, latitude]
     zoom: 12, // Default zoom level to focus on Melbourne
     layers: [],
